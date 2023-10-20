010bd97c5b34cc8ed8cdab691c53c4cdd95a08b2
AlphaVlogger <arunkumaar16.yt@gmail.com>
2023-10-20 18:44:28 +0530

    Main files added

---
+++ LinkTree/links.js
@@ -0,0 +1,40 @@
+export const links = [ 
+ 
+	{ 
+		name: "Linkedin", 
+		link: "https://in.linkedin.com/", 
+		image: 
+"https://as2.ftcdn.net/v2/jpg/03/96/56/19/1000_F_396561934_t1OwjMZHrVY211bHA5uUEAix0F1SyLci.jpg", 
+	}, 
+	{ 
+		name: "Instagram", 
+		link: 
+"https://www.instagram.com/arun_kumaar_yt/?hl=en", 
+		image: 
+"https://p7.hiclipart.com/preview/5/164/976/thepix-digital-marketing-logo-shiftdelete-instagram-logo.jpg", 
+	}, 
+	{ 
+		name: "E-mail", 
+		link: 
+"mailto:arunkumaar16.yt@gmail.com", 
+		image: 
+"https://cdn.pnghd.pics/data/601/gmail-transparent-background-12.png", 
+	}, 
+
+	{ 
+		name: "Github", 
+		link: 
+"https://github.com/AlphaVlogger-YT", 
+		image: 
+"https://pngimg.com/d/github_PNG40.png", 
+	}, 
+
+	{ 
+		name: "Website", 
+		link: 
+"https://alphavloggeryt-inc.000webhostapp.com/", 
+		image: 
+"https://w7.pngwing.com/pngs/117/122/png-transparent-blogger-logo-online-socialmedia-most-usable-logos-icon.png", 
+	}, 
+
+];
