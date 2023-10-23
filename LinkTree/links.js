11627276d3cbafb8f1d981dd2b01bd55a3443781
AlphaVlogger <arunkumaar16.yt@gmail.com>
2023-10-23 22:23:06 +0530

    Updated

--- LinkTree/links.js
+++ LinkTree/links.js
@@ -1,32 +1,32 @@
 export const links = [ 
-
+ 
 	{ 
 		name: "Linkedin", 
 		link: "https://in.linkedin.com/", 
 		image: 
-"https://as2.ftcdn.net/v2/jpg/03/96/56/19/1000_F_396561934_t1OwjMZHrVY211bHA5uUEAix0F1SyLci.jpg", 
+"https://raw.githubusercontent.com/ByteNinjaOfficial/ByteNinjaOfficial.github.io/main/Source/Linkedin_logo.png", 
 	}, 
 	{ 
 		name: "Instagram", 
 		link: 
-"https://www.instagram.com/arun_kumaar_yt/?hl=en", 
+"https://www.instagram.com/arun_kumaar_16/?hl=en", 
 		image: 
-"https://p7.hiclipart.com/preview/5/164/976/thepix-digital-marketing-logo-shiftdelete-instagram-logo.jpg", 
+"https://raw.githubusercontent.com/ByteNinjaOfficial/ByteNinjaOfficial.github.io/main/Source/Instagram_logo.png", 
 	}, 
 	{ 
-		name: "E-mail", 
+		name: "G-mail", 
 		link: 
 "mailto:arunkumaar16.yt@gmail.com", 
 		image: 
-"https://cdn.pnghd.pics/data/601/gmail-transparent-background-12.png", 
+"https://raw.githubusercontent.com/ByteNinjaOfficial/ByteNinjaOfficial.github.io/main/Source/Gmail_logo.png", 
 	}, 
 
 	{ 
 		name: "Github", 
 		link: 
-"https://github.com/AlphaVlogger-YT", 
+"https://github.com/ByteNinjaOfficial", 
 		image: 
-"https://pngimg.com/d/github_PNG40.png", 
+"https://raw.githubusercontent.com/ByteNinjaOfficial/ByteNinjaOfficial.github.io/main/Source/Github_logo.png", 
 	}, 
 
 	{ 
@@ -34,7 +34,7 @@ export const links = [
 		link: 
 "https://alphavloggeryt-inc.000webhostapp.com/", 
 		image: 
-"https://w7.pngwing.com/pngs/117/122/png-transparent-blogger-logo-online-socialmedia-most-usable-logos-icon.png", 
+"https://raw.githubusercontent.com/ByteNinjaOfficial/ByteNinjaOfficial.github.io/main/Source/Wordpress_logo.png", 
 	}, 
 
 ];
