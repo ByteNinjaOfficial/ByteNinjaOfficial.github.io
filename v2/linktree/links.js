167c974afcbda11ec5b11f8c5cc22115620bb103
AlphaVlogger <arunkumaar16.yt@gmail.com>
2024-04-23 11:26:33 +0530

    New changes

---
+++ v2/linktree/links.js
@@ -0,0 +1,40 @@
+export const links = [ 
+ 
+	{ 
+		name: "Linkedin", 
+		link: "https://in.linkedin.com/", 
+		image: 
+"https://raw.githubusercontent.com/ByteNinjaOfficial/ByteNinjaOfficial.github.io/main/Source/Linkedin_logo.png", 
+	}, 
+	{ 
+		name: "Instagram", 
+		link: 
+"https://www.instagram.com/arun_kumaar_16/?hl=en", 
+		image: 
+"https://raw.githubusercontent.com/ByteNinjaOfficial/ByteNinjaOfficial.github.io/main/Source/Instagram_logo.png", 
+	}, 
+	{ 
+		name: "G-mail", 
+		link: 
+"mailto:arunkumaar16.yt@gmail.com", 
+		image: 
+"https://raw.githubusercontent.com/ByteNinjaOfficial/ByteNinjaOfficial.github.io/main/Source/Gmail_logo.png", 
+	}, 
+
+	{ 
+		name: "Github", 
+		link: 
+"https://github.com/ByteNinjaOfficial", 
+		image: 
+"https://raw.githubusercontent.com/ByteNinjaOfficial/ByteNinjaOfficial.github.io/main/Source/Github_logo.png", 
+	}, 
+
+	{ 
+		name: "Website", 
+		link: 
+"https://alphavloggeryt-inc.000webhostapp.com/", 
+		image: 
+"https://raw.githubusercontent.com/ByteNinjaOfficial/ByteNinjaOfficial.github.io/main/Source/Wordpress_logo.png", 
+	}, 
+
+];
