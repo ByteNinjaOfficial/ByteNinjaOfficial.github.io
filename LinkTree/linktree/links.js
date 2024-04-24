99da56c2122b6ede02b1ab0eb31511742f38eb7a
AlphaVlogger <arunkumaar16.yt@gmail.com>
2024-04-24 23:03:28 +0530

    update

---
+++ LinkTree/linktree/links.js
@@ -0,0 +1,40 @@
+export const links = [ 
+ 
+	{ 
+		name: "Linkedin", 
+		link: "https://www.linkedin.com/in/arun-kumaar-aa8611300?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app", 
+		image: 
+"https://raw.githubusercontent.com/ByteNinjaOfficial/ByteNinjaOfficial.github.io/main/LinkTree/img/Linkedin_logo.png", 
+	}, 
+	{ 
+		name: "Instagram", 
+		link: 
+"https://www.instagram.com/arun_kumaar_16/?hl=en", 
+		image: 
+"https://raw.githubusercontent.com/ByteNinjaOfficial/ByteNinjaOfficial.github.io/main/LinkTree/img/Instagram_logo.png", 
+	}, 
+	{ 
+		name: "G-mail", 
+		link: 
+"mailto:arunkumaar16.yt@gmail.com", 
+		image: 
+"https://raw.githubusercontent.com/ByteNinjaOfficial/ByteNinjaOfficial.github.io/main/LinkTree/img/Gmail_logo.png", 
+	}, 
+
+	{ 
+		name: "Github", 
+		link: 
+"https://github.com/ByteNinjaOfficial", 
+		image: 
+"https://raw.githubusercontent.com/ByteNinjaOfficial/ByteNinjaOfficial.github.io/main/LinkTree/img/Github_logo.png", 
+	}, 
+
+	{ 
+		name: "Website", 
+		link: 
+"https://alphavloggeryt-inc.000webhostapp.com/", 
+		image: 
+"https://raw.githubusercontent.com/ByteNinjaOfficial/ByteNinjaOfficial.github.io/main/LinkTree/img/Wordpress_logo.png", 
+	}, 
+
+];
